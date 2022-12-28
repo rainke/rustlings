95eb7ec3bb2cccdf5b8d90ec77d0c1b7fbe338a9
Rainke <woshihepeng520@163.com>
2022-12-28 11:59:25 +0000

    --amend

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -9,31 +9,33 @@
 // implementing this trait.
 // Execute `rustlings hint traits1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
-}
-
-impl AppendBar for String {
-    //Add your code here
-}
-
-fn main() {
+  }
+  
+  impl AppendBar for String {
+    fn append_bar(mut self) -> String {
+        self.push_str("Bar");
+        self
+    }
+  }
+  
+  fn main() {
     let s = String::from("Foo");
     let s = s.append_bar();
     println!("s: {}", s);
-}
-
-#[cfg(test)]
-mod tests {
+  }
+  
+  #[cfg(test)]
+  mod tests {
     use super::*;
-
+  
     #[test]
     fn is_foo_bar() {
         assert_eq!(String::from("Foo").append_bar(), String::from("FooBar"));
     }
-
+  
     #[test]
     fn is_bar_bar() {
         assert_eq!(
@@ -41,4 +43,5 @@ mod tests {
             String::from("BarBar")
         );
     }
-}
+  }
+  
--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -11,22 +11,29 @@
 // you can do this!
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
-}
-
-//TODO: Add your code here
-
-#[cfg(test)]
-mod tests {
+  }
+  
+  //TODO: Add your code here
+  
+  impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Vec<String> {
+      self.push(String::from("Bar"));
+      self
+    }
+  }
+  
+  #[cfg(test)]
+  mod tests {
     use super::*;
-
+  
     #[test]
     fn is_vec_pop_eq_bar() {
         let mut foo = vec![String::from("Foo")].append_bar();
         assert_eq!(foo.pop().unwrap(), String::from("Bar"));
         assert_eq!(foo.pop().unwrap(), String::from("Foo"));
     }
-}
+  }
+  
